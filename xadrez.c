d39521f9ad9b029e575d1433864f26c8a06ad5b3
keila <kbkeilabarbosa@gmail.com>
2024-09-11 19:49:31 +0000

    nivel aventureiro, movimento cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
+    // Nível Aventureiro - Movimentação das Peças
     const int mov_bispo = 5; //casas na diagonal superior direita
     const int mov_torre = 5; //casas para a direita
     const int mov_rainha = 8;// casas para a esquerda
+    const int mov_cavalo_baixo = 2, mov_cavalo_esquerda = 1; //o cavalo move 3 peças formando L
 
     //variavel controle
     int i;
+    int j;
     
     // Implementação de Movimentação da Torre
     printf("Movimentação da torre\n");
@@ -29,6 +28,22 @@ int main() {
     for(i = 1; i <= mov_bispo; i++){
          printf("Bispo moveu %d na diagonal superior direita (cima e direita)\n", i);
     }
+    //Implementação de Movimentação do Cavalo
+    //O cavalo move para baixo e para a esquerda
+    printf("Movimentação do Cavalo\n");
+    for(i =1; i <= mov_cavalo_baixo; i++){
+        //loop externo imprime o movimento vertical(baixo)
+        printf("O Cavalo moveu %d para baixo\n", i);
+        int j = 1;
+        while (j <= mov_cavalo_esquerda)
+        {//loop interno imprime movimento horizontal(esqueda)
+            printf("O Cavalo moveu %d para a esquerda\n", j);
+            j++;
+        }
+        
+        printf("\n");
+    }
+   
 
 
 
