126805bf4affd2e594079180a0a04af4bf92d6ea
keila <kbkeilabarbosa@gmail.com>
2024-09-11 01:38:44 +0000

    movimento bispo, torre, rainha

--- xadrez.c
+++ xadrez.c
@@ -6,27 +6,33 @@
 
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    const int mov_bispo = 5; //casas na diagonal superior direita
+    const int mov_torre = 5; //casas para a direita
+    const int mov_rainha = 8;// casas para a esquerda
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    //variavel controle
+    int i;
+    
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("Movimentação da torre\n");
+    for(i = 1; i <= mov_torre; i++){
+        printf("Torre moveu %d para a direita\n", i);
+    }
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("Movimentação da Rainha\n");
+    for(i = 1; i <= mov_rainha; i++){
+         printf("Rainha moveu %d para a esquerda\n", i);
+    }
+    // Implementação de Movimentação do Bispo
+   printf("Movimentação do Bispo\n");
+    for(i = 1; i <= mov_bispo; i++){
+         printf("Bispo moveu %d na diagonal superior direita (cima e direita)\n", i);
+    }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    
 
     return 0;
 }
