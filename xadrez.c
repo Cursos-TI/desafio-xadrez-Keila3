e7267ae1238510202c2ed984de983e02a1533770
keila <kbkeilabarbosa@gmail.com>
2024-09-12 20:17:33 +0000

    NIVEL MESTRE: uso de void

--- xadrez.c
+++ xadrez.c
@@ -1,47 +1,64 @@
 #include <stdio.h>
+void recursivo(int movimento){
+    // Verifica se o movimento é 5 para a torre
+    if (movimento == 5) {
+        printf("Torre moveu %d casas para a direita\n", movimento);
+    }
+    // Verifica se o movimento é 8 para a rainha
+    else if (movimento == 8) {
+        printf("Rainha moveu %d casas para a esquerda\n", movimento);
+    }
+ }
+ c
+
+    
+
 
 // Desafio de Xadrez - MateCheck
 int main() {
-    // Nível Aventureiro - Movimentação das Peças
+    // Nível Mestre - Movimentação das Peças
     const int mov_bispo = 5; //casas na diagonal superior direita
     const int mov_torre = 5; //casas para a direita
     const int mov_rainha = 8;// casas para a esquerda
-    const int mov_cavalo_baixo = 2, mov_cavalo_esquerda = 1; //o cavalo move 3 peças formando L
+    const int mov_cavalo_cima = 2, mov_cavalo_direita = 1; //o cavalo move 3 peças formando L
 
     //variavel controle
     int i;
     int j;
     
     // Implementação de Movimentação da Torre
-    printf("Movimentação da torre\n");
-    for(i = 1; i <= mov_torre; i++){
-        printf("Torre moveu %d para a direita\n", i);
-    }
+     printf("Movimentação da torre\n");
+     recursivo(mov_torre);
 
     // Implementação de Movimentação da Rainha
-    printf("Movimentação da Rainha\n");
-    for(i = 1; i <= mov_rainha; i++){
-         printf("Rainha moveu %d para a esquerda\n", i);
-    }
+     printf("Movimentação da Rainha\n");
+     recursivo(mov_rainha);
+
     // Implementação de Movimentação do Bispo
-   printf("Movimentação do Bispo\n");
-    for(i = 1; i <= mov_bispo; i++){
-         printf("Bispo moveu %d na diagonal superior direita (cima e direita)\n", i);
-    }
+     printf("Movimentação do Bispo\n");
+     recursivo_bispo(mov_bispo);
+
     //Implementação de Movimentação do Cavalo
-    //O cavalo move para baixo e para a esquerda
-    printf("Movimentação do Cavalo\n");
-    for(i =1; i <= mov_cavalo_baixo; i++){
-        //loop externo imprime o movimento vertical(baixo)
-        printf("O Cavalo moveu %d para baixo\n", i);
-        int j = 1;
-        while (j <= mov_cavalo_esquerda)
-        {//loop interno imprime movimento horizontal(esqueda)
-            printf("O Cavalo moveu %d para a esquerda\n", j);
-            j++;
+     printf("Movimentação do Cavalo\n");
+     for(i = 0, j = 0; i <= mov_cavalo_cima || j <= mov_cavalo_direita;){
+        //movimento vertical (cima)
+        if(i <= mov_cavalo_cima){
+            i++;
+            printf("O cavalo moveu %d casas para cima\n", i);
+            //se o movimento para cima estiver completo, continue para o proximo movimento
+            if( i == mov_cavalo_cima){
+            }
         }
-        
-        printf("\n");
+        //movimento horizontal(direita)
+        if(i == mov_cavalo_cima && j <= mov_cavalo_direita){
+            j++;
+            printf("O cavalo moveu %d casa para direita\n", j);
+        } 
+        //quando ambos os movimentos estiverem concluidos, sai do loop
+        if(i == mov_cavalo_cima && j == mov_cavalo_direita){
+            printf("Cavalo moveu 1 vez em L para cima à direita ");
+            break;
+        }
     }
    
 
